5e8737ee1d60e955f7dd750079f803613ff7aeac
erichuizapucp <eric.huiza@pucp.edu.pe>
2019-11-15 23:14:26 -0500

    adding basic template inheritance

--- app.py
+++ app.py
@@ -6,7 +6,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
+
+
+@app.route('/state-of-art')
+def sate_of_art():
+    return render_template('state-of-art.html')
+
+
+@app.route('/dataset')
+def dataset():
+    return render_template('dataset.html')
+
+
+@app.route('/method')
+def method():
+    return render_template('method.html')
+
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+
+@app.route('/references')
+def references():
+    return render_template('references.html')
 
 
 if __name__ == '__main__':
